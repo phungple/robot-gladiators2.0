8b0bf782a5f628314ce872976786565f3fa6ed4a
phungle <phungple2015@gmail.com>
2023-02-27 14:48:05 -0600

    using parseInt() for shop()

--- assets/js/game.js
+++ assets/js/game.js
@@ -141,22 +141,20 @@ var endGame = function () {
 var shop = function(){
     //ask player what they'd like to do
     var shopOptionPrompt = window.prompt(
-        "Would you like to REFILL your Health, UPGRADE your Attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+        "Would you like to REFILL your Health, UPGRADE your Attack, or LEAVE the store? Please enter: 1 for 'REFILL', 2 for'UPGRADE', or 3 for 'LEAVE' to make a choice."
     );
+    shopOptionPrompt = parseInt(shopOptionPrompt);
     // use switch to carry out action
     switch (shopOptionPrompt) {
-        case "REFILL":
-        case "refill":
+        case 1:
             playerInfo.refillHealth();
             break;
         
-        case "UPGRADE":
-        case "upgrade":
+        case 2:
             playerInfo.upgradeAttack();
             break;
 
-        case "LEAVE":
-        case "leave":
+        case 3:
             window.alert("Leaving the store.");
             
             // do nothing, so function will end
